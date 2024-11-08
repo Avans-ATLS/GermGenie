5dae73f6165eaea90f66c482b4b14c5c017d610a
Sander Boden <sanderboden@gmail.com>
2024-11-08 20:09:03 +0100

    n reads in barplots
    created --nreads option (--nr) for adding number of reads to the barplot output

--- src/GermGenie/pipeline.py
+++ src/GermGenie/pipeline.py
@@ -1,9 +1,12 @@
 import subprocess
 import os
 import glob
+import gzip
 
 import pandas as pd
-import plotly.express as px
+from Bio import SeqIO
+from plotly import express as px
+from plotly import graph_objects as go
 
 
 class EMU:
@@ -14,7 +17,9 @@ class EMU:
         db: str,
         threads: int = 2,
         threshold: int = 1,
+        nreads: bool = False,
     ) -> None:
+        
         """Run EMU on samples, write barplots
 
         Args:
@@ -23,6 +28,7 @@ class EMU:
             db (str): Path to EMU database
             threads (int, optional): Number of threads. Defaults to 2.
             threshold (int, optional): %-abundance threshold for visualizing taxa. Defaults to 1.
+            nreads (bool, optional): Visualize number of reads per sample. Defaults to False.
         """
         # assign std vars
         self.input: str = input_dir
@@ -30,11 +36,13 @@ class EMU:
         self.t: int = threads
         self.db: str = db
         self.threshold: int = threshold
+        self.nreads: bool = nreads
         self.assignment: dict[str, list] = {
             "sample": [],
             "assigned": [],
             "unassigned": [],
         }
+        
         # create path for emu results
         self.emu_dir: str = os.path.join(self.output, "emu")
         # make output dirs
@@ -48,11 +56,20 @@ class EMU:
             print("No input files found...")
             os.abort()
 
+
         # run emu on input files
         for fastq in sorted(infiles):
             self.run_emu(fastq)
+        if self.nreads:
+            self.readcounts = {}
+            for fastq in sorted(infiles):
+                self.readcounts[fastq.split("/")[-1].split(".")[0]] = self.count_reads(fastq)
+                
         # merge emu results
         self.df: pd.DataFrame = self.merge_results()
+        # add nreads if true
+        if self.nreads:
+            self.df["reads"] = self.df.apply(lambda x: self.readcounts[x["sample"]], axis=1)
         # plot species
         fig = self.plot(self.species_data())
         fig.write_html(os.path.join(self.output, "species_bar.html"))
@@ -79,6 +96,21 @@ class EMU:
         self.assignment["sample"].append(name)
         self.assignment["assigned"].append(stdout[1].split(" ")[-1])
         self.assignment["unassigned"].append(stdout[0].split(" ")[-1])
+    
+    def count_reads(self, fq: str) -> int:
+        """Count the number of reads in a fastq file
+
+        Args:
+            fq (str): path to fastq file
+
+        Returns:
+            int: number of reads
+        """
+        count = 0
+        with gzip.open(fq, "rt") as fastq:
+            for _ in SeqIO.parse(fastq, "fastq"):
+                count += 1
+        return count
 
     def merge_results(self) -> pd.DataFrame:
         """Merge abundance tsv's from EMU
@@ -116,6 +148,7 @@ class EMU:
             species_df["abundance"] < self.threshold, "species"
         ] = f"Other species <{self.threshold}%"
         species_df = species_df.groupby(["sample", "species"]).sum().reset_index()
+        species_df['reads'] = species_df.apply(lambda x: self.readcounts[x['sample']], axis=1)
 
         return species_df
 
@@ -129,11 +162,12 @@ class EMU:
             genus_df["abundance"] < self.threshold, "genus"
         ] = f"Other genera <{self.threshold}%"
         genus_df = genus_df.groupby(["sample", "genus"]).sum().reset_index()
+        genus_df['reads'] = genus_df.apply(lambda x: self.readcounts[x['sample']], axis=1)
 
         return genus_df
 
     def plot(self, df: pd.DataFrame):
-        return px.bar(
+        fig = px.bar(
             df,
             x="sample",
             y="abundance",
@@ -142,3 +176,28 @@ class EMU:
             title=f"Relative Abundances of {(list(df.columns)[1]).capitalize()}",
             labels={"sample": "Sample Name", "abundance": "Relative Abundance (%)"},
         )
+        fig = go.Figure(fig)
+        
+        fig.add_trace(
+    
+            go.Scatter(
+                x=df["sample"],
+                y=df["reads"],
+                mode="markers",
+                name="Number of reads",
+                marker=dict(color="red", size=20),
+                yaxis="y2"  # Indicates this should use the secondary y-axis
+            )
+        )
+        # Update layout to add a secondary y-axis
+        fig.update_layout(
+            yaxis=dict(title="Relative Abundance (%)", titlefont=dict(color="blue")),
+            yaxis2=dict(
+                title="Number of reads",
+                titlefont=dict(color="red"),
+                overlaying="y",
+                side="right",
+                
+            )
+        )
+        return fig
